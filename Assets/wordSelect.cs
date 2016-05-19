4063fa5c0b2bc450022736e8824aa9789cd3d37e
mariocova3 <mariocova3@gmail.com>
2016-05-18 23:44:46 -0500

    Clicking Text Works!

---
+++ Assets/wordSelect.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wordSelect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+	
+	}
+
+    void OnMouseDown()
+    {
+        Debug.Log("Congratulations! You did that shit by yourself!");
+    }
+}
